05b649fc6838e5b21148af526961cc271cdf384f
José Deyvisson <josedeyvisson03@gmail.com>
2025-08-21 11:50:35 -0300

    add math expressions

--- bin/main.dart
+++ bin/main.dart
@@ -1,14 +1,23 @@
 
-import 'calculadora.dart';
+import 'dart:io';
+import 'package:math_expressions/math_expressions.dart';
 
 void main(List<String> arguments) {
  
-
-  Calculadora calc = Calculadora();
-  calc.run();
-
+  while(true){
+    
+  print("Digite a expressão");
+  try{
+      Expression expressao = Parser().parse(stdin.readLineSync()!);
+      double resultado = expressao.evaluate(EvaluationType.REAL, ContextModel());
+      if(resultado.isInfinite){
+        print("Não é possivel dividir por 0");
+      }else{
+        print("Resultado: $resultado");
+      }
+  }catch (e) {
+      print("Expressão inválida: $e");
+  }
+  }
 
 }
-
-
-
